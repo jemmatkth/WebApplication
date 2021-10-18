aae85b05dac8399362b1f2c95aab7225b7c89c74
jemmat <jemmat@kth.se>
2021-10-18 16:05:30 +0200

    Read&Write pages in place

--- Controllers/MessagesController.cs
+++ Controllers/MessagesController.cs
@@ -12,165 +12,123 @@ using WebApplication.Models.ViewModels;
 
 namespace WebApplication.Controllers
 {
-    public class MessagesController : Controller
-    {
-        private readonly DbContect _context;
-        private readonly UserManager<WebApplicationUser> _userManager;
+	public class MessagesController : Controller
+	{
+		private readonly DbContect _context;
+		private readonly UserManager<WebApplicationUser> _userManager;
 
 
-        public MessagesController(DbContect context, UserManager<WebApplicationUser> userManager)
-        {
-            _context = context;
-            _userManager = userManager;
-        }
+		public MessagesController(DbContect context, UserManager<WebApplicationUser> userManager)
+		{
+			_context = context;
+			_userManager = userManager;
 
-        // GET: Messages
-        public async Task<IActionResult> Index(int? selectedMessage)
-        {
-            var userId = _userManager.GetUserId(User);
-            var viewModel = new MessageViewModel();
+		}
 
-            viewModel.Messages = await _context.Message.Where(i => i.SentToId == userId).OrderBy(i=>i.CreatedByUsername).ToListAsync();
+		// GET: Messages
+		public async Task<IActionResult> Index(int? selectedMessage)
+		{
+			var userId = _userManager.GetUserId(User);
+			var viewModel = new MessageViewModel();
 
-            if (selectedMessage != null)
-            {
-                ViewData["MessageId"] = selectedMessage.Value;
-                viewModel.Message = viewModel.Messages.Where(i => i.Id == selectedMessage.Value).Single();               
-            }
+			viewModel.Messages = await _context.Message.Where(i => i.SentToId == userId).Where(i => i.Status == false).OrderBy(i => i.CreatedByUsername).ToListAsync();
 
-            return View(viewModel);
-        }
+			if (viewModel.Messages != null)
+			{
+				if (selectedMessage != null)
+				{
+					ViewData["MessageId"] = selectedMessage.Value;
+					viewModel.Message = viewModel.Messages.Where(i => i.Id == selectedMessage.Value).Single();
+					var message = _context.Message.Where(i => i.Id == viewModel.Message.Id).Single();
+					message.Status = true;
+					_context.Update(message);
+					await _context.SaveChangesAsync();
+				}
+			}
+			else
+			{
+				viewModel.Messages = null;
+			}
 
-        // GET: Messages/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
+			return View(viewModel);
+		}
 
-            var message = await _context.Message
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (message == null)
-            {
-                return NotFound();
-            }
 
-            return View(message);
-        }
+		// POST: Messages/Create
+		// To protect from overposting attacks, enable the specific properties you want to bind to.
+		// For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> Create(string SendToUserId, string SendToUserUsername, [Bind("Title,Content")] Message message)
+		{
+			if (ModelState.IsValid)
+			{
+				var userId = _userManager.GetUserId(User);
+				var username = await _userManager.FindByIdAsync(userId);
 
-        // GET: Messages/Create
-        public IActionResult Create()
-        {
-            return View();
-        }
+				message.CreatedById = userId;
+				message.CreatedByUsername = username.UserName;
 
-        // POST: Messages/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Title,Content")] Message message)
-        {
-            if (ModelState.IsValid)
-            {
-                var userId = _userManager.GetUserId(User);
-                message.CreatedById = userId;
-                message.SentToId = userId;
-                message.Created = DateTime.Now;
-                _context.Add(message);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            return View(message);
-        }
+				message.SentToId = SendToUserId.ToString();
+				message.SentToUsername = SendToUserUsername;
+				message.Status = false;
+				message.Created = DateTime.Now;
+				_context.Add(message);
+				await _context.SaveChangesAsync();
 
-        // GET: Messages/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
+				return RedirectToAction(nameof(Edit));
+			}
+			return View(message);
+		}
 
-            var message = await _context.Message.FindAsync(id);
-            Console.WriteLine("GET Edit " + id);
-            if (message == null)
-            {
-                return NotFound();
-            }
-            return View(message);
-        }
+		// GET: Messages/Edit/5
+		public async Task<IActionResult> Edit(string? userId)
+		{
+			var viewModel = new CreateMessageViewModel();
+			var users = await _userManager.Users.ToListAsync();
+			viewModel.Users = users;
 
-        // POST: Messages/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,Content,Created,CreatedById,SentToId,Status")] Message message)
-        {
-            if (id != message.Id)
-            {
-                return NotFound();
-            }
+			if (userId != null || !String.IsNullOrEmpty(userId))
+			{
+				viewModel.SendToUser = await _userManager.FindByIdAsync(userId);
+			}
 
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(message);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!MessageExists(message.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            else
-            {
-                Console.WriteLine("Modelstate not valid");
-            }
-            return View(message);
-        }
+			return View(viewModel);
+		}
 
-        // GET: Messages/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
 
-            var message = await _context.Message
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (message == null)
-            {
-                return NotFound();
-            }
+		// GET: Messages/Delete/5
+		public async Task<IActionResult> Delete(int? id)
+		{
+			if (id == null)
+			{
+				return NotFound();
+			}
 
-            return View(message);
-        }
+			var message = await _context.Message
+				.FirstOrDefaultAsync(m => m.Id == id);
+			if (message == null)
+			{
+				return NotFound();
+			}
 
-        // POST: Messages/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var message = await _context.Message.FindAsync(id);
-            _context.Message.Remove(message);
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
+			return View(message);
+		}
 
-        private bool MessageExists(int id)
-        {
-            return _context.Message.Any(e => e.Id == id);
-        }
-    }
+		// POST: Messages/Delete/5
+		[HttpPost, ActionName("Delete")]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> DeleteConfirmed(int id)
+		{
+			var message = await _context.Message.FindAsync(id);
+			_context.Message.Remove(message);
+			await _context.SaveChangesAsync();
+			return RedirectToAction(nameof(Index));
+		}
+
+		private bool MessageExists(int id)
+		{
+			return _context.Message.Any(e => e.Id == id);
+		}
+	}
 }
---
+++ Models/ViewModels/CreateMessageViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication.Areas.Identity.Data;
+
+namespace WebApplication.Models.ViewModels
+{
+	public class CreateMessageViewModel
+	{
+		public List<WebApplicationUser> Users { get; set; }
+		public WebApplicationUser SendToUser { get; set; }
+		public Message Message { get; set; }
+	}
+}
